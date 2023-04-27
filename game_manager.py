dc3ab3e7744713b681d805f460ffe23c0fff735c
Khalid Elassaad <khalidelassaad@gmail.com>
2023-04-27 11:50:19 -0700

    code: intro text

--- game_manager.py
+++ game_manager.py
@@ -4,7 +4,9 @@ from termcolor import colored
 
 
 class Game:
-    def __init__(self):
+    def __init__(self, skip_init=False):
+        if skip_init:
+            return
         self.red_session = agent_api.create_session("red-agent")
         self.blue_session = agent_api.create_session("blue-agent")
         self.red_session_handle = agent_api.get_session_handle(
@@ -38,7 +40,44 @@ class Game:
                 self.chat_to_agent_and_get_response(agent_color, message)
             )
 
+    def print_intro_text(self):
+        intro_text = [
+            "  Welcome to Conflict Resolution Class!  ",
+            "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+            "  Your goal is to resolve a conflict     ",
+            "  between " +
+            colored("Red", "red", "on_white", attrs=['bold']) +
+            colored(" and ", "dark_grey", "on_white") +
+            colored("Blue", "blue", "on_white", attrs=['bold']) +
+            colored(".                  ", "dark_grey", "on_white"),
+            "                                         ",
+            "  Chat with them, ask them questions     ",
+            "  understand their differences, earn     ",
+            "  their trust, and settle their beef.    ",
+            "                                         ",
+            "  Useful Commands:                       ",
+            "      !exit - Exits the game.            ",
+            "      !red  - Talk to " +
+            colored("Red", "red", "on_white", attrs=['bold']) +
+            colored(".               ", "dark_grey", "on_white"),
+            "      !blue  - Talk to " +
+            colored("Blue", "blue", "on_white", attrs=['bold']) +
+            colored(".             ", "dark_grey", "on_white"),
+            "      !inv - Check your inventory.       ",
+            "                                         ",
+            colored("             🍀 Good Luck! 🍀            ",
+                    "green", "on_white", attrs=["bold"]),
+            "                                         ",
+
+
+        ]
+        for line in intro_text:
+            print(colored(line, "dark_grey", "on_white"))
+
+    def start_game(self):
+        self.print_intro_text()
+
 
 if __name__ == "__main__":
-    game = Game()
-    game.rolling_chat("blue")
+    game = Game(skip_init=True)
+    game.start_game()
