33d3f7f35dfbb8909b63354aec174b130b250497
Khalid Elassaad <khalidelassaad@gmail.com>
2023-04-27 10:39:23 -0700

    code: more fixieclient tests, time to remove reqs

--- agent-api.py
+++ agent-api.py
@@ -2,6 +2,7 @@ import requests
 import dotenv
 import json
 import os
+import fixieai
 
 FIXIE_API_KEY = dotenv.dotenv_values(
     ".env")["FIXIE_API_KEY"]
@@ -25,5 +26,11 @@ def talk_to_agent(agent_name, message):
     return json.loads(response.text)["message"]["text"]
 
 
+client = fixieai.FixieClient(FIXIE_API_KEY)
+
 if __name__ == "__main__":
-    print(talk_to_agent("myagent", "Generate a random number between 4 and 34"))
+    # print(talk_to_agent("myagent", "Generate a random number between 4 and 34"))
+    # print(client.get_agent('khalidelassaad/myagent'))
+    session = client.create_session('khalidelassaad/myagent')
+    # session.add_message("Generate a random number between 16 and 65")
+    print(session.get_messages()[-1])
