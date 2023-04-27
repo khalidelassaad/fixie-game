bc50db79e6a75f1aa674ae4af0d1d3560dab5e10
Khalid Elassaad <khalidelassaad@gmail.com>
2023-04-27 09:57:39 -0700

    code: api call, but invalid credentials...

--- agent-api.py
+++ agent-api.py
@@ -2,8 +2,28 @@ import requests
 import dotenv
 import os
 
-print(os.getcwd())
-
 FIXIE_API_KEY = dotenv.dotenv_values(
     ".env")["FIXIE_API_KEY"]
-print(FIXIE_API_KEY)
+FIXIE_USERNAME = dotenv.dotenv_values(
+    ".env")["FIXIE_USERNAME"]
+
+HEADERS = {
+    "Authorization": "Bearer ${}".format(FIXIE_API_KEY),
+    "Content-Type": "application/json"
+}
+
+
+def talk_to_agent(agent_name, message):
+    agent_url = "https://app.fixie.ai/api/agents/{}/{}".format(
+        FIXIE_USERNAME, agent_name)
+    response = requests.post(
+        agent_url,
+        data={"message": {"text": message}},
+        headers=HEADERS
+    )
+    print(FIXIE_API_KEY)
+    return response.text
+
+
+if __name__ == "__main__":
+    print(talk_to_agent("myagent", "Hello, what do you do?"))
